351d943d07045f0eedaeb798b702348b98fe9a12
justin <justin@icit.eu>
2024-10-08 11:41:16 +0200

    init

---
+++ src/file4.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+// Ignoring the return type
+function getNumber(int $number): int
+{
+    return $number;
+}
+
+echo getNumber(1);
---
+++ src/file5.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+///**
+// * @param int[] $numbers
+// * @return int
+// */
+//function sumNumbers(array $numbers): int
+//{
+//    // Incorrect implementation: return type should be int, but returns array
+//    return array_sum($numbers);
+//}
+//
+//echo sumNumbers([1, 2, 3]);
+
+/**
+ * @param int[] $numbers
+ * @return int
+ */
+function sumNumbers(array $numbers): int
+{
+    // Incorrect implementation: return type should be int, but returns array
+    return array_sum($numbers);
+}
+
+echo sumNumbers([1, 2, 3]);
